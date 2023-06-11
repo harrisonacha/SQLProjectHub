3c66ceac6d8cbcf0bcc43148b34053fd242d419e
Harrison Acha <133501783+harrisonacha@users.noreply.github.com>
2023-06-12 00:07:51 +0100

    Create Project Overview.sql

---
+++ FIFA/Project Overview.sql
@@ -0,0 +1,23 @@
+/* As a data analyst working on the FIFA World Cup project over the past five years, my role involved extracting and analyzing data to provide valuable insights into various aspects of the tournament. The project aimed to answer key questions related to the participating countries, game statistics, and team performance. With an expert tone of voice, here is a summary of the findings:
+
+Countries Participating: The analysis revealed the number of countries that participated in the World Cup over the last five years, providing insights into the global representation and diversity of teams.
+
+Game Statistics: The project compiled comprehensive game statistics, including the total number of games played, wins, losses, goals scored, and goals conceded. These metrics provided a comprehensive overview of the performance and competitiveness of each team.
+
+Highest and Lowest Appearances: Through the analysis, we identified the country with the highest and lowest number of appearances in the World Cup during the specified period. This information shed light on the consistent contenders and the emerging teams.
+
+Highest and Lowest Games Played: The project also highlighted the country with the highest and lowest number of games played, showcasing the teams that actively participated or had limited opportunities during the tournament.
+
+Highest and Lowest Games Won: By analyzing the data, we determined the country with the highest and lowest number of games won, indicating the teams that excelled or struggled in securing victories.
+
+Highest and Lowest Games Lost: The analysis identified the country with the highest and lowest number of games lost, offering insights into the teams that faced challenges or demonstrated resilience during matches.
+
+Highest and Lowest Goals For: The project revealed the country with the highest and lowest number of goals scored, providing an understanding of the offensive prowess or limitations of each team.
+
+Highest and Lowest Goals Against: Similarly, the analysis showcased the country with the highest and lowest number of goals conceded, reflecting the defensive strength or vulnerabilities of the respective teams.
+
+Highest and Lowest Goal Difference: By calculating the goal difference for each country, we determined the team with the highest and lowest margin between goals scored and goals conceded, indicating the most dominant and struggling teams.
+
+Highest and Lowest Points: Lastly, the project identified the country with the highest and lowest number of points earned, considering the win, draw, and loss records. This metric served as an overall indicator of team success or challenges.
+
+Overall, the FIFA World Cup analysis project provided comprehensive insights into the performance, statistics, and dynamics of participating countries over the past five years. The findings contributed to a deeper understanding of the tournament and enabled informed discussions and strategic decision-making in the world of international football. */
