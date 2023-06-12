f77be96cdc3924068e15284a39764d4c88f7fc4b
Harrison Acha <133501783+harrisonacha@users.noreply.github.com>
2023-06-12 01:14:08 +0100

    Create Project Overview.sql

---
+++ Maven Toys/Project Overview.sql
@@ -0,0 +1,26 @@
+/*Project Overview: Sales & Inventory Analysis for Maven Toys
+
+As a data analyst, my capstone project focused on analyzing sales and inventory data for Maven Toys, a fictitious chain of toy stores in Mexico. The project aimed to gain valuable insights into product profitability, inventory management, and the impact of out-of-stock products on sales. With a professional tone, here is an overview of the project:
+
+Data Scope:
+The project involved working with comprehensive sales and inventory data, encompassing information about the products, stores, daily sales transactions, and current inventory levels at each Maven Toys location. The dataset provided a rich source of information for analysis and decision-making.
+
+Objective:
+The primary objective of the project was to address key business questions and provide actionable insights to enhance profitability, optimize inventory levels, and minimize lost sales due to product stockouts.
+
+Methodology:
+To achieve the project objectives, I employed SQL queries and data analysis techniques to extract meaningful information from the sales and inventory dataset. The analysis focused on addressing the following critical questions:
+
+1. Product Category Profits:
+Using SQL queries and calculations, I identified the product categories that drove the highest profits for Maven Toys. Additionally, I examined whether these profitable categories were consistent across all store locations, or if certain locations had unique profit-driving categories.
+
+2. Inventory Analysis:
+To evaluate the amount of capital tied up in inventory, I calculated the total value of inventory across all Maven Toys stores. By considering the average daily sales and inventory levels, I determined how long the current inventory would last. This analysis provided insights into inventory turnover and efficiency.
+
+3. Impact of Stockouts on Sales:
+Using SQL queries and sales transaction data, I investigated if sales were being lost due to out-of-stock products at specific Maven Toys locations. By identifying instances of product stockouts and analyzing corresponding sales data, I assessed the financial impact and recommended strategies to minimize lost sales.
+
+Conclusion:
+The sales and inventory analysis for Maven Toys provided crucial insights into product profitability, inventory management, and the impact of stockouts on sales. The project findings empowered Maven Toys to make informed decisions to drive profitability, optimize inventory levels, and ensure customer satisfaction by minimizing lost sales due to product unavailability.
+
+Overall, the project demonstrated the value of data analysis in supporting strategic decision-making for Maven Toys, allowing the company to make data-driven improvements to its sales and inventory management processes. */
