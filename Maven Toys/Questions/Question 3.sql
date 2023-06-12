3af7dc14fe1d531c50e1c9e01fcf50173701babb
Harrison Acha <133501783+harrisonacha@users.noreply.github.com>
2023-06-12 01:10:44 +0100

    Create Question 3.sql

---
+++ Maven Toys/Questions/Question 3.sql
@@ -0,0 +1,61 @@
+/* Q3: Are sales being lost with out-of-stock products at certain locations? */
+ 
+SELECT
+    s.sale_id,
+	i.stock_on_hand,
+    s.date,
+    st.store_location,
+    p.product_name
+FROM
+    sales AS s
+JOIN
+    stores AS st ON s.store_id = st.store_id
+JOIN
+    products AS p ON s.product_id = p.product_id
+LEFT JOIN
+    inventory AS i ON s.store_id = i.store_id AND s.product_id = i.product_id
+WHERE
+    i.stock_on_hand IS NULL
+    OR i.stock_on_hand = 0
+
+
+/* Yes. The data shows that sales are being lost in certain location as a result of out to stock products. Running a further query as shown below, it
+it was discovered that 31866 sales was lost across 4 different locations */
+
+SELECT
+    s.sale_id,
+	i.stock_on_hand,
+    s.date,
+    st.store_location,
+    p.product_name
+FROM
+    sales AS s
+JOIN
+    stores AS st ON s.store_id = st.store_id
+JOIN
+    products AS p ON s.product_id = p.product_id
+LEFT JOIN
+    inventory AS i ON s.store_id = i.store_id AND s.product_id = i.product_id
+WHERE
+    i.stock_on_hand IS NULL
+    OR i.stock_on_hand = 0
+
+/* While further analysis showed that there was sales lost accross the four store locations, Downstown store lost the most sales (19017) 
+and Airport Store had the least lost sales (1569). The query below  provides the business insights*/
+
+SELECT
+    DISTINCT (st.store_location) AS store_location,
+	COUNT (s.sale_id) AS lost_sales
+FROM
+    sales AS s
+JOIN
+    stores AS st ON s.store_id = st.store_id
+JOIN
+    products AS p ON s.product_id = p.product_id
+LEFT JOIN
+    inventory AS i ON s.store_id = i.store_id AND s.product_id = i.product_id
+WHERE
+    i.stock_on_hand IS NULL
+    OR i.stock_on_hand = 0
+GROUP BY st.store_location;
+
