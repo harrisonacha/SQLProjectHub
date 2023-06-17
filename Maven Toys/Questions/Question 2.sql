50ba92d6b5b8d13601e2a9f379db0243ab0c35b4
Harrison Acha <133501783+harrisonacha@users.noreply.github.com>
2023-06-17 16:33:54 +0100

    Update Question 2.sql

--- Maven Toys/Questions/Question 2.sql
+++ Maven Toys/Questions/Question 2.sql
@@ -1,38 +1,33 @@
 /* Q2: How much money is tied up in inventory at the toy stores? How long will it last? */
 	
-SELECT * FROM inventory;
-SELECT * FROM sales;
-
-
 SELECT
-	SUM (stock_on_hand * product_cost) AS amount_in_inventory
-FROM
-	inventory as i
-FULL JOIN
-	products as p
-ON 
-	p.product_id = i.product_id
-WHERE
-    p.product_category = 'Toys';
+		SUM (stock_on_hand * product_cost) AS money_tied_up
+    FROM
+        inventory
+    JOIN products ON products.product_id = inventory.product_id 
+	WHERE
+    product_category = 'Toys'
 	
-/* Paer A Result: $99,861.47 is tied up in inventory at the Toys stores. */
+/* Part A Result: $99,861.47 is tied up in inventory at the Toys stores. */
+
+WITH average_daily_sales AS (
+    SELECT
+        product_id,
+        AVG(units) AS average_daily_sales
+    FROM
+        sales
+    GROUP BY
+        product_id
+)
 
 SELECT
-	AVG(i.stock_on_hand / s.average_daily_sales) AS estimated_days_last
+    AVG(i.stock_on_hand / s.average_daily_sales) AS estimated_days_last
 FROM
     inventory AS i
 JOIN
     products AS p ON p.product_id = i.product_id
 JOIN
-    (
-        SELECT
-            product_id,
-            AVG(units) AS average_daily_sales
-        FROM
-            sales
-        GROUP BY
-            product_id
-    ) AS s ON s.product_id = p.product_id
+    average_daily_sales AS s ON s.product_id = p.product_id
 WHERE
     p.product_category = 'Toys';
 
