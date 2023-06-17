e0087a37ad2c7f4a7c0c721f8b1c056d5b5298f6
Harrison Acha <133501783+harrisonacha@users.noreply.github.com>
2023-06-17 16:29:56 +0100

    Update Question 1.sql

--- Maven Toys/Questions/Question 1.sql
+++ Maven Toys/Questions/Question 1.sql
@@ -1,20 +1,26 @@
 /* Which product categories drive the biggest profits? Is this the same across store locations? */
 
+WITH category_profit AS (
+    SELECT
+        products.product_category,
+        stores.store_location,
+        SUM(products.product_price - products.product_cost) AS category_profit
+    FROM
+        products
+    JOIN inventory ON products.product_id = inventory.product_id
+    JOIN stores ON inventory.store_id = stores.store_id
+    GROUP BY
+        products.product_category,
+        stores.store_location
+)
+
 SELECT
-    p.product_category,
-    s.store_location,
-    SUM(p.product_price - p.product_cost) AS total_profit
+    cp.product_category,
+    cp.store_location,
+    cp.category_profit
 FROM
-    products AS p
-FULL JOIN
-    inventory AS i ON p.product_id = i.product_id
-FULL JOIN
-    stores AS s ON i.store_id = s.store_id
-GROUP BY
-    p.product_category,
-    s.store_location
-ORDER BY
-	total_profit DESC;
+    category_profit AS cp
+ORDER BY category_profit DESC
   
  /* Result: Toys category drives the biggest profits amongst the product categories at Maven Toys. Also, Toys drives the biggest profits across store locations.
  
